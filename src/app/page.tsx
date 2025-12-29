4ec37f3c7d9096f714ef539811b95b92052442e1
victormatia <fvictor1705@gmail.com>
2025-12-29 15:00:13 -0300

    feat: adiciona Header em App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,48 @@
+'use client';
+
+import Home from '@/pages/Home';
 import About from '@/pages/About';
 import Cases from '@/pages/Cases';
 import Contact from '@/pages/Contact';
-import Home from '@/pages/Home';
+import Header from '@/components/Header';
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function App() {
+  const [isVisibleHeader, setIsVisibleHeader] = useState(false);
+  const homeRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!homeRef.current) return;
+
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries;
+      setIsVisibleHeader(!entry.isIntersecting);
+    }, options);
+
+    observer.observe(homeRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <div>
-      <Home />
+    <main>
+      {isVisibleHeader && <Header />}
+
+      <div ref={homeRef}>
+        <Home />
+      </div>
+
       <About />
       <Cases />
       <Contact />
-    </div>
+    </main>
   );
 }
