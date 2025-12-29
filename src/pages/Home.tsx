fc481bb94e06d46d687ab171b911e5e3a965b3ef
victormatia <fvictor1705@gmail.com>
2025-12-29 14:59:56 -0300

    feat: adiciona imagem como background quando o vídeo não estiver disponível

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import { Mouse } from 'lucide-react';
+import alternativeBackground from '../../public/images/ref-home-alternative-background.png';
+import Image from 'next/image';
 
 export default function Home() {
+  const [videoFailed, setVideoFailed] = useState(false);
+
   return (
     <div id="home" className="relative flex h-screen flex-col justify-end-safe gap-8 overflow-hidden p-4 pb-24 md:p-16">
-      <video
-        className="absolute top-0 left-0 z-0 h-screen w-full object-cover"
-        src="/videos/bg-video.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-      />
+      {!videoFailed ? (
+        <video
+          className="absolute top-0 left-0 z-0 h-screen w-full object-cover"
+          src="/videos/bg-video.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          onError={() => setVideoFailed(true)}
+        />
+      ) : (
+        <Image className="absolute top-0 left-0 z-0 h-screen w-full object-cover" src={alternativeBackground} alt="" />
+      )}
 
       <div className="absolute inset-0 bg-[linear-gradient(20deg,rgba(0,0,0,0.8)_10%,transparent)]"></div>
 
